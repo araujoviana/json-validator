68712cfbe9e12b13832b3a2e6b157b6fdabc9cae
araujoviana <63423848+araujoviana@users.noreply.github.com>
2025-01-21 18:33:04 -0300

    Import file text

--- src/lib/Reader.svelte
+++ src/lib/Reader.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { textareaValue } from "./store";
- import Prism from 'prismjs'
 
     const textareaRes: number[] = [10, 50];
     let textareaStatus = "textarea"; // Change color based on results
@@ -17,6 +16,18 @@
             console.error("Invalid JSON", e);
         }
     };
+
+    const handleFile = (event: Event) => {
+        const file = (event.target as HTMLInputElement).files?.[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                $textareaValue = reader.result as string;
+            };
+            reader.readAsText(file);
+        }
+    };
 </script>
 
 <textarea
@@ -28,5 +39,35 @@
     oninput={handleInput}
 ></textarea>
 
+<div class="alt-inputs">
+    <div class="file">
+        <label class="file-label">
+            <input class="file-input" type="file" onchange={handleFile} />
+            <span class="file-cta">
+                <span class="file-icon">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+                        />
+                    </svg>
+                </span>
+                <span class="file-label"> Import from file… </span>
+            </span>
+        </label>
+    </div>
+</div>
+
 <style>
+    .alt-inputs {
+        padding-top: 1rem;
+    }
 </style>
